256d15bb77832dc4db1e34c879556420d4b150bd
unknown <yusufiassidiq@gmail.com>
2020-03-22 16:16:50 +0700

    add login admin and user

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -9,9 +9,9 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="">
-                  <router-link :to="{name: 'home'}">
-                    Laravel Vue Cabai
-                </router-link>
+                    <router-link :to="{name: 'home'}">
+                        Laravel Vue Cabai
+                    </router-link>
                 </a>
                
 
@@ -23,7 +23,34 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        
+                        <!--UNLOGGED-->
+                        <div v-if="!$auth.check()">
+                        <li  v-for="(route, key) in routes.unlogged" v-bind:key="route.path">
+                            <router-link  :to="{ name : route.path }" :key="key">
+                                {{route.name}}
+                            </router-link>
+                        </li>
+                        </div>
+                        <!--LOGGED USER-->
+                        <div v-if="$auth.check(2)">
+                        <li  v-for="(route, key) in routes.user" v-bind:key="route.path">
+                            <router-link  :to="{ name : route.path }" :key="key">
+                                {{route.name}}
+                            </router-link>
+                        </li>
+                        </div>
+                        <!--LOGGED ADMIN-->
+                        <div v-if="$auth.check(1)">
+                        <li  v-for="(route, key) in routes.admin" v-bind:key="route.path">
+                            <router-link  :to="{ name : route.path }" :key="key">
+                                {{route.name}}
+                            </router-link>
+                        </li>
+                        </div>
+                        <!--LOGOUT-->
+                        <li v-if="$auth.check()">
+                            <a href="#" @click.prevent="$auth.logout();clearLocalStorage()">Logout</a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -42,11 +69,41 @@
   export default {
     data() {
       return {
-        //
+        routes: {
+          // UNLOGGED
+          unlogged: [
+            {
+              name: 'Register',
+              path: 'register'
+            },
+            {
+              name: 'Login',
+              path: 'login'
+            }
+          ],          // LOGGED USER
+          user: [
+            {
+              name: 'Dashboard',
+              path: 'dashboard'
+            }
+          ],
+          // LOGGED ADMIN
+          admin: [
+            {
+              name: 'Dashboard',
+              path: 'admin.dashboard'
+            }
+          ]
+        }
       }
     },
     components: {
     //   navigationMenu
+    },
+    methods: {
+      clearLocalStorage(){
+        localStorage.clear();
+      }
     }
   }
 </script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -2,12 +2,12 @@ import VueRouter from 'vue-router'
 
 // Pages
 import Home from './pages/Home'
-// import Register from './pages/Register'
-// import Login from './pages/Login'
-// import Dashboard from './pages/user/Dashboard'
-// import AdminDashboard from './pages/admin/Dashboard'
-// import UserDetails from './pages/admin/userDetails'
-// import UnverifiedDashboard from './pages/user/UnverifiedDashboard'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Dashboard from './pages/user/Dashboard'
+import AdminDashboard from './pages/admin/Dashboard'
+import UserDetails from './pages/admin/userDetails'
+import UnverifiedDashboard from './pages/user/UnverifiedDashboard'
 
 // Routes
 const routes = [
@@ -19,95 +19,95 @@ const routes = [
       auth: undefined
     }
   },
-  // {
-  //   path: '/register',
-  //   name: 'register',
-  //   component: Register,
-  //   meta: {
-  //     auth: false
-  //   }
-  // },
-  // {
-  //   path: '/login',
-  //   name: 'login',
-  //   component: Login,
-  //   meta: {
-  //     auth: false
-  //   }
-  // },
-  // // USER ROUTES
-  // {
-  //   path: '/dashboard/',
-  //   name: 'dashboard',
-  //   component: Dashboard,
-  //   beforeEnter: (to, from, next) => {
-  //     if(to.meta.requiresVerified) {
-  //       console.log("dirouterjs")
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+    meta: {
+      auth: false
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      auth: false
+    }
+  },
+  // USER ROUTES
+  {
+    path: '/dashboard/',
+    name: 'dashboard',
+    component: Dashboard,
+    beforeEnter: (to, from, next) => {
+      if(to.meta.requiresVerified) {
+        console.log("dirouterjs")
 
-  //       // custom payloads
-  //       var datatoken = []
-  //       const url = "/user/getcustompayloads"
-  //       const axiosTest = axios.get
-  //       axiosTest(url).then(function(axiosTestResult){
-  //           datatoken = axiosTestResult.data
-  //           var status = (datatoken.confirmed)
-  //           if(status === 0){
-  //             next('unverified')
-  //           }else{
-  //             next();
-  //           }
-  //           console.log(datatoken.email)
-  //           console.log("kalomuncultandanyasukses")
-  //       });        
-  //     }
-  //   },
-  //   meta: {
-  //     auth: true,
-  //     requiresVerified: true,
-  //   }
-  // },
-  // {
-  //   path: '/unverified',
-  //   name: 'unverifiedDashboard',
-  //   component: UnverifiedDashboard,
-  //   beforeEnter: (to, from, next) => {
-  //     if(to.meta.requiresVerified) {
-  //       var datatoken = []
-  //       const url = "/user/getcustompayloads"
-  //       const axiosTest = axios.get
-  //       axiosTest(url).then(function(axiosTestResult){
-  //       datatoken = axiosTestResult.data
-  //       var status = (datatoken.confirmed)
-  //       if(status === 1){
-  //         next('dashboard')
-  //       }else{
-  //         next();
-  //       }
-  //       });
-  //     }
-  //   },
-  //   meta: {
-  //     auth: true,
-  //     requiresVerified: true,
-  //   }
-  // },
-  // // ADMIN ROUTES
-  // {
-  //   path: '/admin',
-  //   name: 'admin.dashboard',
-  //   component: AdminDashboard,
-  //   meta: {
-  //     auth: {roles: 2, redirect: {name: 'login'}, forbiddenRedirect: '/403'}
-  //   }
-  // },
-  // {
-  //   path: '/:userId/details',
-  //   name: 'userDetails',
-  //   component: UserDetails,
-  //   meta: {
-  //     auth: {roles: 2, redirect: {name: 'login'}, forbiddenRedirect: '/403'}
-  //   }
-  // }
+        // custom payloads
+        var datatoken = []
+        const url = "/user/getcustompayloads"
+        const axiosTest = axios.get
+        axiosTest(url).then(function(axiosTestResult){
+            datatoken = axiosTestResult.data
+            var status = (datatoken.confirmed)
+            if(status === 0){
+              next('unverified')
+            }else{
+              next();
+            }
+            // console.log(datatoken.email)
+            console.log("kalomuncultandanyasukses")
+        });        
+      }
+    },
+    meta: {
+      auth: true,
+      requiresVerified: true,
+    }
+  },
+  {
+    path: '/unverified',
+    name: 'unverifiedDashboard',
+    component: UnverifiedDashboard,
+    beforeEnter: (to, from, next) => {
+      if(to.meta.requiresVerified) {
+        var datatoken = []
+        const url = "/user/getcustompayloads"
+        const axiosTest = axios.get
+        axiosTest(url).then(function(axiosTestResult){
+        datatoken = axiosTestResult.data
+        var status = (datatoken.confirmed)
+        if(status === 1){
+          next('dashboard')
+        }else{
+          next();
+        }
+        });
+      }
+    },
+    meta: {
+      auth: true,
+      requiresVerified: true,
+    }
+  },
+  // ADMIN ROUTES
+  {
+    path: '/admin',
+    name: 'admin.dashboard',
+    component: AdminDashboard,
+    meta: {
+      auth: {roles: 1, redirect: {name: 'login'}, forbiddenRedirect: '/403'}
+    }
+  },
+  {
+    path: '/:userId/details',
+    name: 'userDetails',
+    component: UserDetails,
+    meta: {
+      auth: {roles: 1, redirect: {name: 'login'}, forbiddenRedirect: '/403'}
+    }
+  }
 ]
 
 const router = new VueRouter({
