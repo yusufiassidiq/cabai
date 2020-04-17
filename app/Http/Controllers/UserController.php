019ae2fc071556289d662e6c097c2400e5b767aa
unknown <yusufiassidiq@gmail.com>
2020-04-17 17:41:30 +0700

    api terimaMitra

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,6 @@ class UserController extends Controller
                 'users' => $users->toArray()
             ], 200);
     }    
-    
     public function show(Request $request, $id){
         $user = User::find($id);        
         return response()->json(
@@ -28,33 +27,27 @@ class UserController extends Controller
                 'user' => $user->toArray()
             ], 200);
     }
-
     // public function seeToken(){
     //     $token = JWTAuth::getToken();
     //     $isiToken = JWTAuth::getPayload($token)->toArray();
     //     return $isiToken;
     // }
-
     public function requesteduser(){
         $users = DB::table('users')->where('status', '=', '0')->get();
-        
         return response()->json(
             [
                 'status' => 'success',
                 'users' => $users->toArray()
             ], 200);
     }
-
     public function validateduser(){
         $users = DB::table('users')->where('status', '=', '1')->get();
-        
         return response()->json(
             [
                 'status' => 'success',
                 'users' => $users->toArray()
             ], 200);
     }
-
     public function requestMitra($id){
         $idUser1 = Auth::user()->id;
         $idUser2 = $id;
@@ -82,6 +75,34 @@ class UserController extends Controller
         $kemitraan->user2_id = $idUser2;
         $kemitraan->save();
         return response()->json(['status' => 'success'], 200);
-        
     }
+    public function terimaMitra($id){
+        $idUser1 = Auth::user()->id;
+        $idUser2 = $id;
+        $action_user = Auth::user()->id;
+        
+        //cek id user 1 harus lebih kecil
+        if($idUser2 < $idUser1) {
+            $temp = $idUser2;
+            $idUser2 = $idUser1;
+            $idUser1 = $temp;
+        }
+        
+        //cek action user
+        $checkActionUser = Kemitraan::where('status',0)->where('user1_id',$idUser1)->where('user2_id',$idUser2)->get();
+        if($action_user == $checkActionUser[0]->action_user){
+            return response()->json([
+                'status' => 'failed',
+                'data' => 'Anda tidak bisa menerima permintaan kemitraan yang anda buat sendiri'
+            ]);
+        }
+        $kemitraan = new Kemitraan;
+        $kemitraan->status = 1;
+        $kemitraan->action_user = $action_user;
+        $kemitraan->user1_id = $idUser1;
+        $kemitraan->user2_id = $idUser2;
+        $kemitraan->save();
+        return response()->json(['status' => 'success'], 200);
+    }
+
 }
